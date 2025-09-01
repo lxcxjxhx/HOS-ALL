5868da557327d4aff76783a3556d9492ab8837bd
lxcxjxhx <144213008+lxcxjxhx@users.noreply.github.com>
2025-09-01 09:22:27 +0800

    Delete HOS-MANAGER-ALL directory

--- HOS-MANAGER-ALL/INTEL-SE/test_pdfplumber.py
+++
@@ -1,4 +0,0 @@
-import pdfplumber
-with pdfplumber.open('/home/lxcxjxhx/PROJECT/INTEL-SE/docs/test.pdf') as pdf:
-    text = ''.join(page.extract_text() or '' for page in pdf.pages)
-    print(text)
